1c42f121cea420d4a90ed3d4765d8a06ec6cee38
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2016-05-01 13:46:04 +0200

    'Bubble sort by' completed

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -11,8 +11,34 @@ def bubble_sort (array)
 	end
 	break if !swapped
   end
-  return array
+  array
 end
 
-example = [4,3,78,2,0,2]
-print bubble_sort(example)
+def bubble_sort_by (array)
+  loop do
+    swapped = false
+	pos = 0
+	array.length.-(1).times do
+	  if yield(array[pos], array[pos+1]) > 0
+	    array[pos], array[pos+1] = array[pos+1], array[pos]
+		swapped = true
+	  end
+	  pos += 1
+	end
+	break if !swapped
+  end
+  array
+end
+
+example_i = [4,3,78,2,0,2]
+example_s = ["hi","hello","hey"]
+# asc numeric
+print "#{bubble_sort(example_i)}\n"
+# asc by string length
+print "#{bubble_sort_by(example_s) do |left, right|
+  left.length - right.length
+end}\n"
+# desc by string length
+print "#{bubble_sort_by(example_s) do |left, right|
+  right.length - left.length
+end}\n"
