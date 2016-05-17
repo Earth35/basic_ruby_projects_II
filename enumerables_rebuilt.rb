8d9f853f8699239ba7b17f5b162b40e02ee0f787
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2016-05-17 11:39:05 +0200

    Fixed #my_inject(arg) bug

--- enumerables_rebuilt.rb
+++ enumerables_rebuilt.rb
@@ -61,7 +61,11 @@ module Enumerable
   
   def my_inject (init = self[0])
     result = init
-    self[1..self.length].my_each { |x| result = yield(result, x) }
+    if result == self[0] 
+      self[1..self.length].my_each { |x| result = yield(result, x) }
+    else
+      self.my_each { |x| result = yield(result, x) }
+    end
     return result
   end
   
@@ -83,38 +87,38 @@ module Enumerable
 end
 
 def multiply_els (array)
-    array.my_inject { |sum, el| sum * el }
+  array.my_inject { |sum, el| sum * el }
 end
 
 # tests
-array = [2,3,4]
-my_proc = Proc.new { |x| x ** 2 }
-# my_each
-print("#{array.my_each { |x| puts x }}\n")
-# my_each_with_index
-print(array.my_each_with_index { |x, index| puts "#{index}: #{x}"})
-# my_select
-print("\n#{array.my_select { |x| x > 2 }}\n")
-# my_all?
-print("#{array.my_all? { |x| x > 2 }}\n")
-print("#{array.my_all? { |x| x > 0 }}\n")
-# my_any?
-print("#{array.my_any? { |x| x > 10}}\n")
-print("#{array.my_any? { |x| x > 2}}\n")
-# my_none?
-print("#{array.my_none? { |x| x > 1}}\n")
-print("#{array.my_none? { |x| x > 8}}\n")
-# my_count
-print("#{array.my_count}\n")
-print("#{array.my_count(3)}\n")
-print("#{array.my_count { |x| x > 2 }}\n")
-# my_map
-print("#{array.my_map { |x| x ** 2 }}\n")
-# my_map_v2
-print("#{array.my_map_v2(my_proc)}\n")  # proc same as block above, i.e. x ** 2
-# my_map_v3
-print("#{array.my_map_v3(my_proc) { |x| x ** 3 }}\n")  # proc: x ** 2
-# my_inject
-print("#{array.my_inject { |sum, x| sum + x }}")
-# my_inject via multiply_els
-print("\n#{multiply_els(array)}")
+# array = [2,3,4]
+# my_proc = Proc.new { |x| x ** 2 }
+# # my_each
+# print("#{array.my_each { |x| puts x }}\n")
+# # my_each_with_index
+# print(array.my_each_with_index { |x, index| puts "#{index}: #{x}"})
+# # my_select
+# print("\n#{array.my_select { |x| x > 2 }}\n")
+# # my_all?
+# print("#{array.my_all? { |x| x > 2 }}\n")
+# print("#{array.my_all? { |x| x > 0 }}\n")
+# # my_any?
+# print("#{array.my_any? { |x| x > 10}}\n")
+# print("#{array.my_any? { |x| x > 2}}\n")
+# # my_none?
+# print("#{array.my_none? { |x| x > 1}}\n")
+# print("#{array.my_none? { |x| x > 8}}\n")
+# # my_count
+# print("#{array.my_count}\n")
+# print("#{array.my_count(3)}\n")
+# print("#{array.my_count { |x| x > 2 }}\n")
+# # my_map
+# print("#{array.my_map { |x| x ** 2 }}\n")
+# # my_map_v2
+# print("#{array.my_map_v2(my_proc)}\n")  # proc same as block above, i.e. x ** 2
+# # my_map_v3
+# print("#{array.my_map_v3(my_proc) { |x| x ** 3 }}\n")  # proc: x ** 2
+# # my_inject
+# print("#{array.my_inject { |sum, x| sum + x }}")
+# # my_inject via multiply_els
+# print("\n#{multiply_els(array)}")
